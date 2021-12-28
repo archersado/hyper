436476d962b1f1a8b463865c472502359d752150
archersado <archersado@hotmail.com>
2021-12-28 17:08:10 +0800

    fix: fix containerId

--- packages/react-hyper/src/core/index.tsx
+++ packages/react-hyper/src/core/index.tsx
@@ -37,7 +37,7 @@ const BoardWarp = (props: any) => {
     };
 
     useLayoutEffect(() => {
-        canvas.current = document.getElementById('rpe');
+        canvas.current = document.getElementById(containerId);
         (canvas.current as HTMLElement).dataset.engineType = engineType;
         (canvas.current as HTMLElement).dataset.config = JSON.stringify(config);
         
